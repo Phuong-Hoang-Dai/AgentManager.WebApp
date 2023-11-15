e3a225923541920eff6cbb0a867268b0380591ea
Thuan <quocthuancusghcm@gmail.com>
2023-11-15 10:17:39 +0700

    Thuan

--- AgentManager.WebApp/Controllers/DeptReportController.cs
+++ AgentManager.WebApp/Controllers/DeptReportController.cs
@@ -28,34 +28,52 @@ namespace AgentManager.WebApp.Controllers
         // GET: Agent/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-           
+
 
             if (id == null || _context.Agents == null)
-                {
-                    return NotFound();
-                }
-                     
+            {
+                return NotFound();
+            }
 
-                var agent = await _context.Agents
-                    .Include(a => a.AgentCategory)
-                    .Include(a => a.District)
-                    .Include(a => a.DeliveryNotes)
-                    .ThenInclude(b => b.Staff)
-                    .FirstOrDefaultAsync(m => m.AgentId == id);
+
+            var agent = await _context.Agents
+                .Include(a => a.AgentCategory)
+                .Include(a => a.District)
+                .Include(a => a.DeliveryNotes)
+                .ThenInclude(b => b.Staff)
+                .FirstOrDefaultAsync(m => m.AgentId == id);
 
 
             if (agent == null)
-                {
-                    return NotFound();
-                }
+            {
+                return NotFound();
+            }
 
             decimal totalPrice = agent.DeliveryNotes.Sum(dn => dn.TotalPrice);
             ViewBag.TotalPrice = totalPrice;
 
             return View(agent);
-            
+
         }
-       
+
+
+
+        //Báo cáo doanh thu
+        public IActionResult Revenue()
+        {
+            List<Receipt> receipts = _context.Receipts.Include(p => p.Agent).ToList();
+           //ứng với mỗi cash là doanh thu 1 tr
+            foreach (var receipt in receipts)
+            {
+                receipt.Cash *= 1000000;
+                
+            }
+            //đẩy receipt qua view
+            ViewBag.Revenue = receipts;
+            //đẩy totalcasd qua view
+            ViewData["sumtotal"] = receipts.Sum(receipt => receipt.Cash);
+            return View();
+        }
 
     }
 }
